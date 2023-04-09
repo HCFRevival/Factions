b65c3e4086b8bbfe88d3ef0c15a210ebf0aa21fb
j-rockwell <john@llewkcor.com>
2023-04-09 16:47:59 -0700

    main: added loadPlayer, savePlayer and savePlayers function

--- src/main/java/gg/hcfactions/factions/player/PlayerManager.java
+++ src/main/java/gg/hcfactions/factions/player/PlayerManager.java
@@ -1,16 +1,26 @@
 package gg.hcfactions.factions.player;
 
 import com.google.common.collect.Sets;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import gg.hcfactions.factions.Factions;
 import gg.hcfactions.factions.manager.IManager;
 import gg.hcfactions.factions.models.player.IFactionPlayer;
+import gg.hcfactions.factions.models.player.impl.FactionPlayer;
+import gg.hcfactions.libs.base.connect.impl.mongo.Mongo;
 import lombok.Getter;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bukkit.entity.Player;
 
 import java.util.Set;
 import java.util.UUID;
 
 public final class PlayerManager implements IManager {
+    public static final String PLAYER_DB_NAME = "dev";
+    public static final String PLAYER_DB_COLL_NAME = "players";
+
     @Getter public Factions plugin;
     @Getter public Set<IFactionPlayer> playerRepository;
 
@@ -25,8 +35,66 @@ public final class PlayerManager implements IManager {
 
     @Override
     public void onDisable() {
+        savePlayers();
+
         this.plugin = null;
         this.playerRepository = null;
+    }
+
+    public IFactionPlayer loadPlayer(Bson filter, boolean cache) {
+        final Mongo mdb = (Mongo) plugin.getConnectable(Mongo.class);
+        if (mdb == null) {
+            plugin.getAresLogger().error("attempted to load player with null mongo instance");
+            return null;
+        }
+
+        final MongoDatabase db = mdb.getDatabase(PLAYER_DB_NAME);
+        if (db == null) {
+            plugin.getAresLogger().error("attempted to load player with null db instance");
+            return null;
+        }
+
+        final MongoCollection<Document> coll = db.getCollection(PLAYER_DB_COLL_NAME);
+        final Document doc = coll.find(filter).first();
+        if (doc == null) {
+            return null;
+        }
+
+        final FactionPlayer factionPlayer = new FactionPlayer().fromDocument(doc);
+        if (cache) {
+            playerRepository.add(factionPlayer);
+        }
+
+        return factionPlayer;
+    }
+
+    public void savePlayer(IFactionPlayer factionPlayer) {
+        final FactionPlayer fp = (FactionPlayer) factionPlayer;
+        final Mongo mdb = (Mongo) plugin.getConnectable(Mongo.class);
+        if (mdb == null) {
+            plugin.getAresLogger().error("attempted to save player with null mongo instance");
+            return;
+        }
+
+        final MongoDatabase db = mdb.getDatabase(PLAYER_DB_NAME);
+        if (db == null) {
+            plugin.getAresLogger().error("attempted to save player with null db instance");
+            return;
+        }
+
+        final MongoCollection<Document> coll = db.getCollection(PLAYER_DB_COLL_NAME);
+        final Document existing = coll.find(Filters.eq("uuid", fp.getUniqueId().toString())).first();
+
+        if (existing != null) {
+            coll.replaceOne(existing, fp.toDocument());
+        } else {
+            coll.insertOne(fp.toDocument());
+        }
+    }
+
+    public void savePlayers() {
+        playerRepository.forEach(this::savePlayer);
+        plugin.getAresLogger().info("wrote " + playerRepository.size() + " players to db");
     }
 
     public IFactionPlayer getPlayer(UUID uniqueId) {
