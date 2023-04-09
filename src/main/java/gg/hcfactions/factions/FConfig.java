652615880bf2d9f50e22020bdb57900f8ee951fb
j-rockwell <john@llewkcor.com>
2023-04-09 16:46:35 -0700

    main: added spawn locations to config

--- src/main/java/gg/hcfactions/factions/FConfig.java
+++ src/main/java/gg/hcfactions/factions/FConfig.java
@@ -1,11 +1,18 @@
 package gg.hcfactions.factions;
 
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 
 import java.util.Arrays;
 import java.util.List;
 
 public final class FConfig {
+    // world
+    @Getter public Location overworldSpawn;
+    @Getter public Location netherSpawn;
+    @Getter public Location endSpawn;
+
     // faction naming
     @Getter public int minFactionNameLength;
     @Getter public int maxFactionNameLength;
@@ -18,6 +25,10 @@ public final class FConfig {
 
     // TODO: load values from config
     public void loadConfig() {
+        overworldSpawn = new Location(Bukkit.getWorld("world"), 0.0, 64.0, 0.0);
+        netherSpawn = new Location(Bukkit.getWorld("world_nether"), 0.0, 64.0, 0.0);
+        endSpawn = new Location(Bukkit.getWorld("world_the_end"), 0.0, 64.0, 0.0);
+
         minFactionNameLength = 3;
         maxFactionNameLength = 16;
         disallowedFactionNames = Arrays.asList("test");
