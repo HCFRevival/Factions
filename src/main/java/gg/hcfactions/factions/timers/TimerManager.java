71843b71012c312e123c6d05869a4df8e015c6a7
j-rockwell <john@llewkcor.com>
2023-04-09 21:35:18 -0700

    main: impl timer hud

--- src/main/java/gg/hcfactions/factions/timers/TimerManager.java
+++ src/main/java/gg/hcfactions/factions/timers/TimerManager.java
@@ -1,17 +1,28 @@
 package gg.hcfactions.factions.timers;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import gg.hcfactions.factions.Factions;
 import gg.hcfactions.factions.manager.IManager;
 import gg.hcfactions.factions.models.faction.impl.PlayerFaction;
+import gg.hcfactions.factions.models.player.impl.FactionPlayer;
 import gg.hcfactions.libs.base.timer.impl.GenericTimer;
+import gg.hcfactions.libs.base.util.Time;
 import gg.hcfactions.libs.bukkit.scheduler.Scheduler;
 import lombok.Getter;
 import lombok.Setter;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
+
+import java.util.List;
 
 public final class TimerManager implements IManager {
     @Getter public final Factions plugin;
     @Getter @Setter public BukkitTask updateTask;
+    @Getter @Setter public BukkitTask uiTask;
 
     public TimerManager(Factions plugin) {
         this.plugin = plugin;
@@ -19,6 +30,29 @@ public final class TimerManager implements IManager {
 
     @Override
     public void onEnable() {
+        uiTask = new Scheduler(plugin).sync(() -> {
+            plugin.getPlayerManager().getPlayerRepository().stream().filter(p -> !p.getTimers().isEmpty()).forEach(p -> {
+                final List<String> toRender = Lists.newArrayList();
+
+                p.getTimers().stream().filter(t -> t.getType().isRender()).forEach(rt -> {
+                    final String time = (rt.getType().isDecimal() && rt.getRemainingSeconds() < 10)
+                            ? Time.convertToDecimal(rt.getRemaining()) + "s"
+                            : Time.convertToHHMMSS(rt.getRemaining());
+
+                    toRender.add(rt.getType().getDisplayName() + ChatColor.RED + ": " + time);
+                });
+
+                // TODO: Append event timers here
+                // TODO: Append automatic reboots here
+
+                final Player player = ((FactionPlayer)p).getBukkit();
+                if (!toRender.isEmpty() && player != null) {
+                    final String hud = Joiner.on(ChatColor.RESET + " " + ChatColor.RESET + " ").join(toRender);
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(hud));
+                }
+            });
+        }).repeat(0L, 1L).run();
+
         updateTask = new Scheduler(plugin).sync(() -> {
             plugin.getPlayerManager().getPlayerRepository()
                     .stream()
@@ -38,7 +72,9 @@ public final class TimerManager implements IManager {
 
     @Override
     public void onDisable() {
+        uiTask.cancel();
         updateTask.cancel();
+        uiTask = null;
         updateTask = null;
     }
 }
