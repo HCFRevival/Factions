7bd2ee5843e4ff0d449bd866eff19b650cce6d98
j-rockwell <john@llewkcor.com>
2023-04-10 11:00:09 -0700

    main: impl teleportToSafety#

--- src/main/java/gg/hcfactions/factions/utils/FactionUtil.java
+++ src/main/java/gg/hcfactions/factions/utils/FactionUtil.java
@@ -1,8 +1,12 @@
 package gg.hcfactions.factions.utils;
 
 import gg.hcfactions.factions.Factions;
+import gg.hcfactions.libs.bukkit.location.impl.PLocatable;
+import gg.hcfactions.libs.bukkit.scheduler.Scheduler;
 import gg.hcfactions.libs.bukkit.utils.Players;
 import org.bukkit.entity.Player;
+
+import java.util.Objects;
 
 public final class FactionUtil {
     public static void cleanPlayer(Factions plugin, Player player) {
@@ -11,6 +15,18 @@ public final class FactionUtil {
     }
 
     public static void teleportToSafety(Factions plugin, Player player) {
+        final PLocatable location = new PLocatable(player);
 
+        new Scheduler(plugin).async(() -> {
+            while (plugin.getClaimManager().getClaimAt(location) != null) {
+                location.setX(location.getX() + 1.0);
+                location.setZ(location.getZ() + 1.0);
+            }
+
+            new Scheduler(plugin).sync(() -> {
+                location.setY(Objects.requireNonNull(location.getBukkitLocation().getWorld()).getHighestBlockYAt(location.getBukkitLocation()));
+                player.teleport(location.getBukkitLocation());
+            }).run();
+        }).run();
     }
 }
