32bc96a923c96c42486e0b19c0244818a6b62e5d
j-rockwell <john@llewkcor.com>
2023-04-11 18:07:09 -0700

    main: added permission to debug command

--- src/main/java/gg/hcfactions/factions/cmd/DebugCommand.java
+++ src/main/java/gg/hcfactions/factions/cmd/DebugCommand.java
@@ -1,14 +1,17 @@
 package gg.hcfactions.factions.cmd;
 
+import gg.hcfactions.factions.FPermissions;
 import gg.hcfactions.factions.models.logger.impl.CombatLogger;
 import gg.hcfactions.libs.acf.BaseCommand;
 import gg.hcfactions.libs.acf.annotation.CommandAlias;
+import gg.hcfactions.libs.acf.annotation.CommandPermission;
 import gg.hcfactions.libs.acf.annotation.Subcommand;
 import org.bukkit.entity.Player;
 
 @CommandAlias("debug")
-public class DebugCommand extends BaseCommand {
+public final class DebugCommand extends BaseCommand {
     @Subcommand("cl")
+    @CommandPermission(FPermissions.P_FACTIONS_ADMIN)
     public void onCombatLoggerSpawn(Player player) {
         final CombatLogger logger = new CombatLogger(player.getLocation(), player, 30);
         logger.spawn();
