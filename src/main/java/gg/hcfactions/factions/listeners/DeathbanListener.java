bc05a77516f916ced31a4483f177546f80f16623
j-rockwell <john@llewkcor.com>
2023-05-11 12:48:28 -0700

    dev: added event zone deathban cap

--- src/main/java/gg/hcfactions/factions/listeners/DeathbanListener.java
+++ src/main/java/gg/hcfactions/factions/listeners/DeathbanListener.java
@@ -2,22 +2,28 @@ package gg.hcfactions.factions.listeners;
 
 import gg.hcfactions.factions.Factions;
 import gg.hcfactions.factions.listeners.events.player.CombatLoggerDeathEvent;
+import gg.hcfactions.factions.models.claim.impl.Claim;
+import gg.hcfactions.factions.models.events.IEvent;
+import gg.hcfactions.factions.models.faction.impl.ServerFaction;
 import gg.hcfactions.factions.models.logger.impl.CombatLogger;
 import gg.hcfactions.factions.models.state.EServerState;
 import gg.hcfactions.factions.models.stats.EStatisticType;
 import gg.hcfactions.factions.models.stats.impl.PlayerStatHolder;
 import gg.hcfactions.factions.state.ServerStateManager;
+import gg.hcfactions.libs.bukkit.location.impl.PLocatable;
 import gg.hcfactions.libs.bukkit.services.impl.deathbans.event.PlayerDeathbanEvent;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 public record DeathbanListener(@Getter Factions plugin) implements Listener {
@@ -27,6 +33,16 @@ public record DeathbanListener(@Getter Factions plugin) implements Listener {
         final UUID uniqueId = player.getUniqueId();
         final ServerStateManager stateManager = plugin.getServerStateManager();
         final PlayerStatHolder statHolder = plugin.getStatsManager().getPlayerStatistics(uniqueId);
+        final Claim insideClaim = plugin.getClaimManager().getClaimAt(new PLocatable(player));
+        boolean isInEvent = false;
+
+        if (insideClaim != null && plugin.getFactionManager().getFactionById(insideClaim.getOwner()) instanceof final ServerFaction sf) {
+            final Optional<IEvent> eventQuery = plugin.getEventManager().getEvent(sf);
+
+            if (eventQuery.isPresent() && eventQuery.get().isActive()) {
+                isInEvent = true;
+            }
+        }
 
         if (statHolder == null) {
             return;
@@ -36,6 +52,7 @@ public record DeathbanListener(@Getter Factions plugin) implements Listener {
                 uniqueId,
                 event.getDeathMessage(),
                 (int)(statHolder.getStatistic(EStatisticType.PLAYTIME) / 1000L),
+                isInEvent,
                 stateManager.getCurrentState().equals(EServerState.SOTW),
                 (stateManager.getCurrentState().equals(EServerState.EOTW_PHASE_1) || stateManager.getCurrentState().equals(EServerState.EOTW_PHASE_2))
         );
@@ -46,7 +63,21 @@ public record DeathbanListener(@Getter Factions plugin) implements Listener {
     @EventHandler (priority = EventPriority.MONITOR)
     public void onCombatLoggerDeath(CombatLoggerDeathEvent event) {
         final CombatLogger logger = event.getLogger();
+        final Location location = logger.getBukkitEntity().getLocation();
         final UUID uniqueId = logger.getOwnerId();
+        final Claim insideClaim = plugin.getClaimManager().getClaimAt(
+                new PLocatable(Objects.requireNonNull(location.getWorld()).getName(), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch()));
+        boolean inEvent = false;
+
+        if (insideClaim != null && plugin.getFactionManager().getFactionById(insideClaim.getOwner()) instanceof final ServerFaction sf) {
+            final Optional<IEvent> eventQuery = plugin.getEventManager().getEvent(sf);
+
+            if (eventQuery.isPresent() && eventQuery.get().isActive()) {
+                inEvent = true;
+            }
+        }
+
+        final boolean isInEvent = inEvent;
 
         plugin.getStatsManager().getPlayerStatistics(uniqueId, holder -> {
             if (holder == null) {
@@ -57,6 +88,7 @@ public record DeathbanListener(@Getter Factions plugin) implements Listener {
                     uniqueId,
                     ChatColor.RED + "Your combat-logger was slain by " + ChatColor.YELLOW + event.getKiller().getName(),
                     (int)(holder.getStatistic(EStatisticType.PLAYTIME) / 1000L),
+                    isInEvent,
                     plugin.getServerStateManager().getCurrentState().equals(EServerState.SOTW),
                     (plugin.getServerStateManager().getCurrentState().equals(EServerState.EOTW_PHASE_1) || plugin.getServerStateManager().getCurrentState().equals(EServerState.EOTW_PHASE_2))
             );
