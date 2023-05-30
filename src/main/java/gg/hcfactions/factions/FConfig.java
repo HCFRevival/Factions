87a42ce8ef39ec3a8122d6fc56a609b62897f5c9
j-rockwell <john@llewkcor.com>
2023-05-30 15:04:11 -0700

    dev: config.yml overhaul

--- src/main/java/gg/hcfactions/factions/FConfig.java
+++ src/main/java/gg/hcfactions/factions/FConfig.java
@@ -2,7 +2,6 @@ package gg.hcfactions.factions;
 
 import gg.hcfactions.factions.models.state.EServerState;
 import gg.hcfactions.factions.stats.StatsConfig;
-import gg.hcfactions.libs.bukkit.location.impl.PLocatable;
 import gg.hcfactions.libs.bukkit.services.impl.deathbans.DeathbanConfig;
 import gg.hcfactions.libs.bukkit.utils.Configs;
 import lombok.Getter;
@@ -40,15 +39,9 @@ public final class FConfig {
     @Getter public double powerCap;
     @Getter public int powerTickInterval;
     @Getter public int powerTickPlayerModifier;
-    @Getter public boolean reducePowerLossInNether;
-    @Getter public boolean reducePowerLossInEnd;
-
-    // faction ratings
-    @Getter public int challengerRating;
-    @Getter public int diamondRating;
-    @Getter public int platinumRating;
-    @Getter public int goldRating;
-    @Getter public int silverRating;
+    @Getter public double netherPowerLossReduction;
+    @Getter public double endPowerLossReduction;
+    @Getter public double eventPowerLossReduction;
 
     // faction limits
     @Getter public int maxFactionSize;
@@ -94,9 +87,11 @@ public final class FConfig {
     @Getter public int lifeUseDelay;
     @Getter public String shopUrl;
 
-    // display stuffs
+    // display
     @Getter public String scoreboardTitle;
     @Getter public String scoreboardFooter;
+
+    @Getter public int displayUpdateInterval;
 
     // server states
     @Getter public EServerState initialServerState;
@@ -108,6 +103,9 @@ public final class FConfig {
 
     // economy
     @Getter public double startingBalance;
+
+    // lunar
+    @Getter public boolean useLegacyLunarAPI;
 
     public FConfig(Factions plugin) {
         this.plugin = plugin;
@@ -142,83 +140,133 @@ public final class FConfig {
     public void loadConfig() {
         final YamlConfiguration conf = plugin.loadConfiguration("config");
 
-        final PLocatable endExitLoc = Configs.parsePlayerLocation(conf, "factions.spawns.end_exit");
-        final PLocatable endSpawnLoc = Configs.parsePlayerLocation(conf, "factions.spawns.end_spawn");
-        final PLocatable overworldSpawnLoc = Configs.parsePlayerLocation(conf, "factions.spawns.overworld_spawn");
-
-        endExit = endExitLoc.getBukkitLocation();
-        endSpawn = endSpawnLoc.getBukkitLocation();
-        overworldSpawn = overworldSpawnLoc.getBukkitLocation();
-
-        mongoUri = conf.getString("databases.mongodb.uri");
-        mongoDatabaseName = conf.getString("databases.mongodb.database");
+        mongoUri = conf.getString("databases.mongo.uri");
+        mongoDatabaseName = conf.getString("databases.mongo.database");
         redisUri = conf.getString("databases.redis.uri");
+        plugin.getAresLogger().info("Using MongoDB Database: " + mongoDatabaseName);
 
-        minFactionNameLength = conf.getInt("factions.naming.min_faction_name");
-        maxFactionNameLength = conf.getInt("factions.naming.max_faction_name");
-        disallowedFactionNames = conf.getStringList("factions.naming.disallowed_names");
-
+        maxFactionSize = conf.getInt("factions.max_faction_size");
         defaultFactionReinvites = conf.getInt("factions.reinvites");
+        minFactionNameLength = conf.getInt("factions.name.min_length");
+        maxFactionNameLength = conf.getInt("factions.name.max_length");
+        disallowedFactionNames = conf.getStringList("factions.name.disallowed");
+        plugin.getAresLogger().info("Max Faction Size: " + maxFactionSize);
+        plugin.getAresLogger().info("Faction Re-invites: " + defaultFactionReinvites);
+        plugin.getAresLogger().info("Minimum Faction Name Length: " + minFactionNameLength);
+        plugin.getAresLogger().info("Maximum Faction Name Length: " + maxFactionNameLength);
+        plugin.getAresLogger().info("Disallowed Faction Names: " + disallowedFactionNames.size());
 
-        maxFactionSize = conf.getInt("factions.limits.max_faction_size");
+        archerClassLimit = conf.getInt("factions.class_limits.archer");
+        bardClassLimit = conf.getInt("factions.class_limits.bard");
+        rogueClassLimit = conf.getInt("factions.class_limits.rogue");
+        minerClassLimit = conf.getInt("factions.class_limits.miner");
+        diverClassLimit = conf.getInt("factions.class_limits.diver");
+        plugin.getAresLogger().info("Archer Class Limit: " + archerClassLimit);
+        plugin.getAresLogger().info("Bard Class Limit: " + bardClassLimit);
+        plugin.getAresLogger().info("Rogue Class Limit: " + rogueClassLimit);
+        plugin.getAresLogger().info("Diver Class Limit: " + diverClassLimit);
+        plugin.getAresLogger().info("Miner Class Limit: " + minerClassLimit);
 
-        playerPowerValue = conf.getDouble("factions.power.player_power_value");
         powerCap = conf.getDouble("factions.power.max_power");
+        playerPowerValue = conf.getDouble("factions.power.player_power_value");
         powerTickInterval = conf.getInt("factions.power.power_tick_interval");
-        powerTickPlayerModifier = conf.getInt("factions.power_tick_player_modifier");
-        reducePowerLossInNether = conf.getBoolean("factions.power.power_loss_reductions.nether");
-        reducePowerLossInEnd = conf.getBoolean("factions.power.power_loss_reduction.end");
+        netherPowerLossReduction = conf.getDouble("factions.power.power_loss_reductions.nether");
+        endPowerLossReduction = conf.getDouble("factions.power.power_loss_reductions.end");
+        eventPowerLossReduction = conf.getDouble("factions.power.power_loss_reductions.event");
+        plugin.getAresLogger().info("Max Faction Power: " + powerCap);
+        plugin.getAresLogger().info("Player Power Value: " + playerPowerValue);
+        plugin.getAresLogger().info("Power Tick Interval (Base): " + powerTickInterval);
+        plugin.getAresLogger().info("Nether Power Loss Reduction: " + netherPowerLossReduction);
+        plugin.getAresLogger().info("The End Power Loss Reduction: " + endPowerLossReduction);
+        plugin.getAresLogger().info("Event Power Loss Reduction: " + eventPowerLossReduction);
 
-        challengerRating = conf.getInt("factions.ratings.challenger");
-        diamondRating = conf.getInt("factions.ratings.diamond");
-        platinumRating = conf.getInt("factions.ratings.platinum");
-        goldRating = conf.getInt("factions.ratings.gold");
-        silverRating = conf.getInt("factions.ratings.silver");
-
-        defaultServerFactionBuildBuffer = conf.getInt("factions.claiming.default_buffers.server_faction_build");
-        defaultServerFactionClaimBuffer = conf.getInt("factions.claiming.default_buffers.server_faction_claim");
-        defaultPlayerFactionClaimBuffer = conf.getInt("factions.claiming.default_buffers.player_faction_claim");
-        claimBlockValue = conf.getDouble("factions.claiming.block_value");
-        claimMinSize = conf.getInt("factions.claiming.min_claim_size");
+        claimMinSize = conf.getInt("factions.claiming.min_size");
         claimMaxAmount = conf.getInt("factions.claiming.max_claims");
-        claimRefundPercent = conf.getDouble("factions.claiming.refunded_percent");
+        claimBlockValue = conf.getDouble("factions.claiming.block_value");
+        claimRefundPercent = conf.getDouble("factions.claiming.refund_percentage");
+        plugin.getAresLogger().info("Minimum Claim Size: " + claimMinSize);
+        plugin.getAresLogger().info("Max Claim Amount: " + claimMaxAmount);
+        plugin.getAresLogger().info("Claim Block Value: " + claimBlockValue);
+        plugin.getAresLogger().info("Claim Refund Percentage: " + claimRefundPercent);
 
-        attackerCombatTagDuration = conf.getInt("factions.timers.attacker_tag_duration");
-        attackedCombatTagDuration = conf.getInt("factions.timers.attacked_tag_duration");
-        enderpearlDuration = conf.getInt("factions.timers.enderpearl_duration");
-        crappleDuration = conf.getInt("factions.timers.crapple_duration");
-        gappleDuration = conf.getInt("factions.timers.gapple_duration");
-        totemDuration = conf.getInt("factions.timers.totem_duration");
-        tridentDuration = conf.getInt("factions.timers.trident_duration");
-        stuckDuration = conf.getInt("factions.timers.stuck_duration");
-        rallyDuration = conf.getInt("factions.timers.rally_duration");
-        freezeDuration = conf.getInt("factions.timers.freeze_duration");
-        homeDuration = conf.getInt("factions.timers.home_duration");
-        sotwProtectionDuration = conf.getInt("factions.timers.protection.sotw");
-        normalProtectionDuration = conf.getInt("factions.timers.protection.normal");
+        defaultServerFactionBuildBuffer = conf.getInt("factions.claiming.buffer_values.server_build");
+        defaultServerFactionClaimBuffer = conf.getInt("factions.claiming.buffer_values.server_claim");
+        defaultPlayerFactionClaimBuffer = conf.getInt("factions.claiming.buffer_values.player_claim");
+        plugin.getAresLogger().info("Server Faction Build Buffer: " + defaultServerFactionBuildBuffer);
+        plugin.getAresLogger().info("Server Faction Claim Buffer: " + defaultServerFactionClaimBuffer);
+        plugin.getAresLogger().info("Player Faction Claim Buffer: " + defaultPlayerFactionClaimBuffer);
 
-        minerClassLimit = conf.getInt("classes.limits.miner");
-        archerClassLimit = conf.getInt("classes.limits.archer");
-        bardClassLimit = conf.getInt("classes.limits.bard");
-        rogueClassLimit = conf.getInt("classes.limits.rogue");
-        diverClassLimit = conf.getInt("classes.limits.diver");
+        freezeDuration = conf.getInt("factions.timers.freeze");
+        rallyDuration = conf.getInt("factions.timers.rally");
+        plugin.getAresLogger().info("Faction Freeze Timer Duration: " + freezeDuration);
+        plugin.getAresLogger().info("Faction Rally Timer Duration: " + rallyDuration);
 
-        scoreboardTitle = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(conf.getString("scoreboard.title")));
-        scoreboardFooter = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(conf.getString("scoreboard.footer")));
+        overworldSpawn = Configs.parsePlayerLocation(conf, "spawns.overworld").getBukkitLocation();
+        endSpawn = Configs.parsePlayerLocation(conf, "spawns.end_spawn").getBukkitLocation();
+        endExit = Configs.parsePlayerLocation(conf, "spawns.end_exit").getBukkitLocation();
+        plugin.getAresLogger().info("Overworld Spawn set to: " + overworldSpawn.toString());
+        plugin.getAresLogger().info("End Spawn set to: " + endSpawn.toString());
+        plugin.getAresLogger().info("End Exit set to: " + endExit.toString());
+
+        attackerCombatTagDuration = conf.getInt("player.timers.combat_tag.attacker");
+        attackedCombatTagDuration = conf.getInt("player.timers.combat_tag.attacked");
+        enderpearlDuration = conf.getInt("player.timers.enderpearl");
+        crappleDuration = conf.getInt("player.timers.crapple");
+        gappleDuration = conf.getInt("player.timers.gapple");
+        totemDuration = conf.getInt("player.timers.totem");
+        tridentDuration = conf.getInt("player.timers.trident");
+        stuckDuration = conf.getInt("player.timers.stuck");
+        homeDuration = conf.getInt("player.timers.home");
+        normalProtectionDuration = conf.getInt("player.timers.protection.normal");
+        sotwProtectionDuration = conf.getInt("player.timers.protection.sotw");
+        plugin.getAresLogger().info("Combat Tag (Attacker) Duration: " + attackerCombatTagDuration);
+        plugin.getAresLogger().info("Combat Tag (Attacked) Duration: " + attackedCombatTagDuration);
+        plugin.getAresLogger().info("Enderpearl Duration: " + enderpearlDuration);
+        plugin.getAresLogger().info("Crapple Duration: " + crappleDuration);
+        plugin.getAresLogger().info("Gapple Duration: " + gappleDuration);
+        plugin.getAresLogger().info("Totem Duration: " + totemDuration);
+        plugin.getAresLogger().info("Trident Duration: " + tridentDuration);
+        plugin.getAresLogger().info("Stuck Duration: " + stuckDuration);
+        plugin.getAresLogger().info("Home Duration: " + homeDuration);
+        plugin.getAresLogger().info("Protection (Normal) Duration: " + normalProtectionDuration);
+        plugin.getAresLogger().info("Protection (SOTW) Duration: " + sotwProtectionDuration);
 
         deathbansEnabled = conf.getBoolean("deathbans.enabled");
         deathbansStandalone = conf.getBoolean("deathbans.standalone");
-        eventDeathbanDuration = conf.getInt("deathbans.max_durations.event");
-        sotwMaxDeathbanDuration = conf.getInt("deathbans.max_durations.sotw");
-        normalMaxDeathbanDuration = conf.getInt("deathbans.max_durations.normal");
+        sotwMaxDeathbanDuration = conf.getInt("deathbans.ban_durations.sotw");
+        normalMaxDeathbanDuration = conf.getInt("deathbans.ban_durations.normal");
+        eventDeathbanDuration = conf.getInt("deathbans.ban_durations.event");
         minDeathbanDuration = conf.getInt("deathbans.min_duration");
         lifeUseDelay = conf.getInt("deathbans.life_use_delay");
         shopUrl = conf.getString("deathbans.shop_url");
+        plugin.getAresLogger().info("Deathbans Enabled: " + deathbansEnabled);
+        plugin.getAresLogger().info("Deathbans running in Standalone: " + deathbansStandalone);
+        plugin.getAresLogger().info("Deathban Duration (SOTW): " + sotwMaxDeathbanDuration);
+        plugin.getAresLogger().info("Deathban Duration (NORMAL): " + normalMaxDeathbanDuration);
+        plugin.getAresLogger().info("Deathban Duration (EVENT): " + eventDeathbanDuration);
+        plugin.getAresLogger().info("Minimum Deathban Duration: " + minDeathbanDuration);
+        plugin.getAresLogger().info("Life Use Delay: " + lifeUseDelay);
+        plugin.getAresLogger().info("Shop URL: " + shopUrl);
 
-        initialServerState = EServerState.fromString(conf.getString("server_state.current_state"));
-        eotwBorderShrinkRadius = conf.getDouble("server_state.eotw.border_shrink_radius");
-        eotwBorderShrinkRate = conf.getInt("server_state.eotw.border_shrink_rate");
+        mapNumber = conf.getInt("stats.map");
+        plugin.getAresLogger().info("Stats tracked under Map: " + mapNumber);
 
-        startingBalance = conf.getDouble("economy.starting_balance");
+        startingBalance = conf.getInt("economy.starting_balance");
+        plugin.getAresLogger().info("Economy Starting Balance: " + startingBalance);
+
+        scoreboardTitle = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(conf.getString("scoreboard.title")));
+        scoreboardFooter = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(conf.getString("scoreboard.footer")));
+        plugin.getAresLogger().info("Scoreboard Title: " + scoreboardTitle);
+        plugin.getAresLogger().info("Scoreboard Footer: " + scoreboardFooter);
+
+        initialServerState = EServerState.fromString(conf.getString("state.current"));
+        eotwBorderShrinkRadius = conf.getDouble("state.eotw.border_shrink_radius");
+        eotwBorderShrinkRate = conf.getInt("state.eotw.border_shrink_rate");
+        plugin.getAresLogger().info("Initial Server State: " + initialServerState.name());
+        plugin.getAresLogger().info("EOTW Border Shrink Radius: " + eotwBorderShrinkRadius);
+        plugin.getAresLogger().info("EOTW Border Shrink Rate: " + eotwBorderShrinkRate);
+
+        useLegacyLunarAPI = conf.getBoolean("lunar_api.use_legacy");
+        plugin.getAresLogger().info("Use Legacy Lunar API: " + useLegacyLunarAPI);
     }
 }
