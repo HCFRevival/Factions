4aa68129bd1d15140c20f803f0540e6533517f21
j-rockwell <john@llewkcor.com>
2023-06-08 08:56:09 -0700

    dev: moved sound queue to global for rogue backstab + added heart particles in

--- src/main/java/gg/hcfactions/factions/listeners/CosmeticsListener.java
+++ src/main/java/gg/hcfactions/factions/listeners/CosmeticsListener.java
@@ -3,7 +3,10 @@ package gg.hcfactions.factions.listeners;
 import com.google.common.collect.Sets;
 import gg.hcfactions.factions.Factions;
 import gg.hcfactions.factions.listeners.events.player.CombatLoggerDeathEvent;
+import gg.hcfactions.factions.models.faction.impl.PlayerFaction;
+import gg.hcfactions.factions.models.message.FMessage;
 import gg.hcfactions.libs.bukkit.scheduler.Scheduler;
+import gg.hcfactions.libs.bukkit.services.impl.ranks.RankService;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -11,10 +14,13 @@ import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.block.Skull;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -85,5 +91,34 @@ public final class CosmeticsListener implements Listener {
         new Scheduler(plugin).sync(() -> recentlyPerformedHeadLookup.remove(uniqueId)).delay(20L).run();
 
         player.sendMessage(ChatColor.YELLOW + skull.getOwningPlayer().getName() + "'s Head");
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (!event.getEntity().getType().equals(EntityType.ENDER_DRAGON)) {
+            return;
+        }
+
+        final LivingEntity dragon = event.getEntity();
+        final Player killer = dragon.getKiller();
+
+        if (killer != null) {
+            final RankService rankService = (RankService) plugin.getService(RankService.class);
+            final PlayerFaction pf = plugin.getFactionManager().getPlayerFactionByPlayer(killer);
+
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                final StringBuilder displayName = new StringBuilder(rankService.getFormattedName(killer));
+
+                if (pf != null) {
+                    displayName.insert(0, (pf.isMember(player)
+                            ? ChatColor.DARK_GREEN + "[" + pf.getName() + "] "
+                            : FMessage.LAYER_2 + "[" + FMessage.LAYER_1 + pf.getName() + FMessage.LAYER_2 + "] "));
+                }
+
+                player.sendMessage(" ");
+                player.sendMessage(ChatColor.DARK_PURPLE + "The " + ChatColor.LIGHT_PURPLE + "Ender Dragon" + ChatColor.DARK_PURPLE + " has been slain by " + displayName);
+                player.sendMessage(" ");
+            }
+        }
     }
 }
