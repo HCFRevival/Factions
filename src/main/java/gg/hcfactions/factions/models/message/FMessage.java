2078b284b48eb678e7001b45ef5495e87d77b39f
j-rockwell <john@llewkcor.com>
2023-06-24 20:07:08 -0700

    dev: added click to teleport to combat logger message

--- src/main/java/gg/hcfactions/factions/models/message/FMessage.java
+++ src/main/java/gg/hcfactions/factions/models/message/FMessage.java
@@ -306,6 +306,16 @@ public final class FMessage {
                         .create());
     }
 
+    public static void printStaffCombatLogger(Player viewer, String username, Location location) {
+        viewer.spigot().sendMessage(
+                new ComponentBuilder("[Teleport to " + username + "'s Combat Logger]")
+                        .color(net.md_5.bungee.api.ChatColor.GRAY)
+                        .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tp " + location.getBlockX() + " " + location.getY() + " " + location.getZ() + " " + Objects.requireNonNull(location.getWorld()).getName()))
+                        .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to teleport").create()))
+                        .create()
+        );
+    }
+
     public static List<String> getEnemyNametag(String username, String factionName) {
         final List<String> res = Lists.newArrayList();
 
