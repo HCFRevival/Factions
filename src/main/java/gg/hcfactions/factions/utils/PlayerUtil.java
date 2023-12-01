484ad94be829e72668c15f21180efd66ad81742e
j-rockwell <john@llewkcor.com>
2023-11-30 17:19:15 -0800

    dev(feat): added hasMythicItem#

--- src/main/java/gg/hcfactions/factions/utils/PlayerUtil.java
+++ src/main/java/gg/hcfactions/factions/utils/PlayerUtil.java
@@ -1,8 +1,14 @@
 package gg.hcfactions.factions.utils;
 
+import gg.hcfactions.factions.Factions;
+import gg.hcfactions.factions.items.mythic.IMythicItem;
+import gg.hcfactions.libs.bukkit.services.impl.items.CustomItemService;
+import gg.hcfactions.libs.bukkit.services.impl.items.ICustomItem;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Optional;
 
 public final class PlayerUtil {
     /**
@@ -23,5 +29,40 @@ public final class PlayerUtil {
         }
 
         return null;
+    }
+
+    /**
+     * Searches a players inventory to find the first ItemStack that is a Mythic Item
+     * and returns true if the Mythic Item is found.
+     * @param plugin Factions Plugin
+     * @param player Player
+     * @return True if player has a Mythic in their inventory
+     */
+    public static boolean hasMythicItem(Factions plugin, Player player) {
+        final CustomItemService cis = (CustomItemService) plugin.getService(CustomItemService.class);
+
+        if (cis == null) {
+            return false;
+        }
+
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item == null) {
+                continue;
+            }
+
+            final Optional<ICustomItem> customItemQuery = cis.getItem(item);
+
+            if (customItemQuery.isEmpty()) {
+                continue;
+            }
+
+            final ICustomItem customItem = customItemQuery.get();
+
+            if (customItem instanceof IMythicItem) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
