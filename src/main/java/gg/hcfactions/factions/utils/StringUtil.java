7e9227764ff604bce944702b75b602831cbc7e36
j-rockwell <john@llewkcor.com>
2023-11-30 21:25:11 -0800

    dev(feat): added getMythicEmblem# and getRomanNumeral

--- src/main/java/gg/hcfactions/factions/utils/StringUtil.java
+++ src/main/java/gg/hcfactions/factions/utils/StringUtil.java
@@ -3,11 +3,12 @@ package gg.hcfactions.factions.utils;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 
 import java.util.List;
 
 public final class StringUtil {
-    public static List<String> formatLore(List<String> lore, String entry, ChatColor color) {
+    public static void formatLore(List<String> lore, String entry, ChatColor color) {
         final String[] split = entry.split(" ");
         final List<String> merged = Lists.newArrayList();
 
@@ -29,7 +30,31 @@ public final class StringUtil {
             final String mergeString = Joiner.on(" ").join(merged);
             lore.add(color + mergeString);
         }
+    }
 
-        return lore;
+    public static String getMythicEmblem(Material material) {
+        String emblem = "\uD83D\uDDE1"; // Sword
+
+        if (material.name().endsWith("_AXE")) {
+            emblem = "\uD83E\uDE93"; // Axe
+        } else if (material.equals(Material.BOW) || material.equals(Material.CROSSBOW)) {
+            emblem = "\uD83C\uDFF9"; // Bow
+        } else if (material.equals(Material.TRIDENT)) {
+            emblem = "\uD83D\uDD31"; // Trident
+        }
+
+        return emblem;
+    }
+
+    public static String getRomanNumeral(int level) {
+        final String[] thousands = {"", "M", "MM", "MMM"};
+        final String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        final String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        final String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return thousands[level / 1000] +
+                hundreds[(level % 1000) / 100] +
+                tens[(level % 100) / 10] +
+                ones[level % 10];
     }
 }
