1dd64dd8590fa7b8b581e714af283c5fcd832d16
j-rockwell <john@llewkcor.com>
2024-04-30 18:35:52 -0700

    1.20.5 - Adventure API support for FScoreboard and added Ally field

--- src/main/java/gg/hcfactions/factions/models/player/EScoreboardEntryType.java
+++ src/main/java/gg/hcfactions/factions/models/player/EScoreboardEntryType.java
@@ -2,15 +2,17 @@ package gg.hcfactions.factions.models.player;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.format.NamedTextColor;
 
+@Getter
 @AllArgsConstructor
 public enum EScoreboardEntryType {
-    FRIENDLY("friendly", ChatColor.DARK_GREEN, true, true),
-    FOCUS("focus", ChatColor.LIGHT_PURPLE, false, false);
+    FRIENDLY("friendly", NamedTextColor.DARK_GREEN, true, true),
+    ALLY("ally", NamedTextColor.BLUE, false, false),
+    FOCUS("focus",  NamedTextColor.LIGHT_PURPLE, false, false);
 
-    @Getter public final String scoreboardTeamName;
-    @Getter public final ChatColor color;
-    @Getter public final boolean canAlwaysSeeNametag;
-    @Getter public final boolean canSeeInvisibles;
+    public final String scoreboardTeamName;
+    public final NamedTextColor color;
+    public final boolean canAlwaysSeeNametag;
+    public final boolean canSeeInvisibles;
 }
