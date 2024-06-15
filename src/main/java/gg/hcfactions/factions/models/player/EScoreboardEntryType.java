eb1e2b1253a6e0f3af3bb043a7c1578854ff69bb
j-rockwell <john@llewkcor.com>
2024-06-14 19:00:12 -0700

    dev: impl obfuscation

--- src/main/java/gg/hcfactions/factions/models/player/EScoreboardEntryType.java
+++ src/main/java/gg/hcfactions/factions/models/player/EScoreboardEntryType.java
@@ -7,12 +7,14 @@ import net.kyori.adventure.text.format.NamedTextColor;
 @Getter
 @AllArgsConstructor
 public enum EScoreboardEntryType {
-    FRIENDLY("friendly", NamedTextColor.DARK_GREEN, true, true),
-    ALLY("ally", NamedTextColor.BLUE, false, false),
-    FOCUS("focus",  NamedTextColor.LIGHT_PURPLE, false, false);
+    FRIENDLY("friendly", NamedTextColor.DARK_GREEN, true, true, false),
+    ALLY("ally", NamedTextColor.BLUE, false, false, false),
+    FOCUS("focus",  NamedTextColor.LIGHT_PURPLE, false, false, false),
+    OBFUSCATED("obfuscated", NamedTextColor.WHITE, false, true, true);
 
     public final String scoreboardTeamName;
     public final NamedTextColor color;
     public final boolean canAlwaysSeeNametag;
     public final boolean canSeeInvisibles;
+    public final boolean obfuscated;
 }
